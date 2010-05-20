975f910dcb9efb11e3ed5a27077989595d22c969
D4 <d4@xero.com>
2010-05-21 09:24:45 +1200

    Fix ConsumerRequest.ToString() bugfix; Add Xero.ScreenCast

---
+++ source/Xero.ScreenCast/Helpers/StringExtensions.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using System.Xml;
+
+namespace XeroScreencast.Helpers
+{
+    public static class StringExtensions
+    {
+
+        public static string ReadSingleNode(this string xml, string nodeXPath)
+        {
+            XmlDocument xmlDocument = new XmlDocument();
+            xmlDocument.LoadXml(xml);
+
+            XmlNode node = xmlDocument.SelectSingleNode(nodeXPath);
+            return (node == null) ? string.Empty : node.InnerText;
+        }
+
+        public static IEnumerable<string> ReadNodes(this string xml, string nodeXPath)
+        {
+            XmlDocument xmlDocument = new XmlDocument();
+            xmlDocument.LoadXml(xml);
+
+            foreach (XmlNode node in xmlDocument.SelectNodes(nodeXPath))
+            {
+                yield return node.InnerText;
+            }
+        }
+
+    }
+}
---
+++ source/Xero.ScreenCast/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace XeroScreencast
+{
+    public class Program
+    {
+
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Do you want to run as a public or private application?");
+            Console.WriteLine(" Enter 1 for a public application");
+            Console.WriteLine(" Enter 2 for a private application");
+
+            ConsoleKeyInfo keyInfo = Console.ReadKey(true);
+            Console.WriteLine();
+
+            if (keyInfo.KeyChar == '1')
+            {
+                Console.WriteLine("Running as a public application...");
+                PublicApps.Run();
+            }
+            if (keyInfo.KeyChar == '2')
+            {
+                Console.WriteLine("Running as a private application...");
+                PrivateApps.Run();
+            }
+
+            Console.WriteLine("");
+            Console.WriteLine(" Press Enter to Exit");
+            Console.ReadLine();
+        }
+
+    }
+}
